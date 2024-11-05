30243ca9e2249e97b8084357d61400fbce6ca5fa
Cristóbal Bustos <cbustos@zerus.cl>
2024-11-05 23:21:05 +0000

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Permite cualquier hostname
+        port: '',       // Sin especificar puerto (cualquier puerto o ninguno)
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
         source: '/',
-        destination: '/login', // Ruta real (puede ser diferente)
+        destination: '/login',
       },
     ];
   },
