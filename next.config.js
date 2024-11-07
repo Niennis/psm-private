1a7fc61eef6f6ac479f3493dda6ea2218a8f76eb
cbustoso <121638773+cbustoso@users.noreply.github.com>
2024-11-07 02:59:18 -0300

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,22 +1,12 @@
-const express = require('express');
-const app = express();
-
-app.use((req, res, next) => {
-  if (req.url === '/') {
-    return res.redirect(302, '/login');
-  }
-  next();
-});
-
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
 module.exports = {
   async rewrites() {
     return [
       {
         source: '/',
-        destination: '/login', // Ruta real (puede ser diferente)
+        destination: '/login',
+        options: {
+          redirectStatusCode: 302,
+        },
       },
     ];
   },
