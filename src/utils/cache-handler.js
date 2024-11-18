cc9065266745458cd7710e847cf87e643f5deb2e
Estefania <estefania.osses.v@gmail.com>
2024-11-18 09:30:26 -0300

    Add error handlers to cache config

--- src/utils/cache-handler.js
+++ src/utils/cache-handler.js
@@ -6,22 +6,37 @@ module.exports = class CacheHandler {
   }
 
   async get(key) {
-    return cache.get(key);
+    try {
+      return cache.get(key);
+    } catch (error) {
+      console.error(`Error al obtener la clave ${key} del caché:`, error);
+      throw error;
+    }
   }
 
   async set(key, data, ctx) {
-    cache.set(key, {
-      value: data,
-      lastModified: Date.now(),
-      tags: ctx ? ctx.tags : [],
-    });
+    try {
+      cache.set(key, {
+        value: data,
+        lastModified: Date.now(),
+        tags: ctx ? ctx.tags : [],
+      });
+    } catch (error) {
+      console.error(`Error al guardar la clave ${key} en el caché:`, error);
+      throw error;
+    }
   }
 
   async revalidateTag(tag) {
-    for (let [key, value] of cache) {
-      if (value.tags.includes(tag)) {
-        cache.delete(key);
+    try {
+      for (let [key, value] of cache) {
+        if (value.tags.includes(tag)) {
+          cache.delete(key);
+        }
       }
+    } catch (error) {
+      console.error(`Error al revalidar la etiqueta ${tag} en el caché:`, error);
+      throw error;
     }
   }
 };
