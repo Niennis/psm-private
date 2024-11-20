ba0686e72b8ab665ef084384aa80a5958d91fe04
Estefania <estefania.osses.v@gmail.com>
2024-11-20 19:48:16 -0300

    Filters for professionals

--- src/utils/getDoctorsWithDespeje.js
+++ src/utils/getDoctorsWithDespeje.js
@@ -1,18 +1,63 @@
 import { fetchProfessionals } from '../services/DoctorsServices';
 import { fetchScheduleByAvailability } from '@/services/SchedulesServices';
 
-export const obtenerDoctoresDespeje = async () => {
+export const fetchProfDespeje = async () => {
   const doctores = await fetchProfessionals();
   // const doctoresConDespeje = [];
 
   // for (const doctor of doctores) {
   //   const { users: bloques } = await fetchScheduleByAvailability(doctor.id);
-  //   const tieneDespeje = bloques.some(bloque => bloque.tipo && bloque.tipo.includes('despeje'));
-  //   console.log('tieneDespeje', tieneDespeje)
+  //   const tieneDespeje = bloques.some(bloque => bloque.tipoServicio && bloque.tipoServicio.includes('despeje'));
   //   if (tieneDespeje) {
   //     doctoresConDespeje.push(doctor);
   //   }
   // }
   // return doctoresConDespeje;
   return doctores;
+};
+
+
+export const FetchProfAcompañamiento = async () => {
+  const doctores = await fetchProfessionals();
+  const doctoresConDespeje = [];
+
+  for (const doctor of doctores) {
+    const { users: bloques } = await fetchScheduleByAvailability(doctor.id);
+    const tieneDespeje = bloques.some(bloque => bloque.tipoServicio && bloque.tipoServicio.includes('Acompañamiento'));
+    if (tieneDespeje) {
+      doctoresConDespeje.push(doctor);
+    }
+  }
+  return doctoresConDespeje;
+  // return doctores;
+};
+
+export const fetchProfPsicoBreve = async () => {
+  const doctores = await fetchProfessionals();
+  const doctoresConDespeje = [];
+
+  for (const doctor of doctores) {
+    const { users: bloques } = await fetchScheduleByAvailability(doctor.id);
+    const tieneDespeje = bloques.some(bloque => bloque.tipoServicio && bloque.tipoServicio.includes('breve'));
+    if (tieneDespeje) {
+      doctoresConDespeje.push(doctor);
+    }
+  }
+  return doctoresConDespeje;
+  // return doctores;
+};
+
+export const fetchProfPsicoIndividual = async () => {
+  const doctores = await fetchProfessionals();
+  const doctoresConDespeje = [];
+
+  for (const doctor of doctores) {
+    const { users: bloques } = await fetchScheduleByAvailability(doctor.id);
+    const tieneDespeje = bloques.some(bloque => bloque.tipoServicio && bloque.tipoServicio.includes('individual'));
+    if (tieneDespeje) {
+      doctoresConDespeje.push(doctor);
+    }
+  }
+  return doctoresConDespeje;
+  // return doctores;
 };
