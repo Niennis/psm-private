b71a97db3d444388730044d5c4ca8f2ad58a861d
Estefania <estefania.osses.v@gmail.com>
2024-11-25 23:23:24 -0300

    Remove standalone configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   cacheHandler: require.resolve('./src/utils/cache-handler.js'),
-  output: 'standalone',
-  distDir: 'build', // Mantener el estándar.
+  // distDir: '.next', // Mantener el estándar.
   compress: true, // Habilita compresión por defecto.
   images: {
     remotePatterns: [
