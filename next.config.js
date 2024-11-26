0aa4b8dd5e25417caf1fea8f1125e038ed75eed5
Estefania <estefania.osses.v@gmail.com>
2024-11-25 21:30:29 -0300

    Cache management

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   cacheHandler: require.resolve('./src/utils/cache-handler.js'),
-  distDir: '.next', // Mantener el estándar.
+  // distDir: '.next', // Mantener el estándar.
   compress: true, // Habilita compresión por defecto.
   images: {
     remotePatterns: [
@@ -10,5 +10,30 @@ module.exports = {
         port: '',
       },
     ],
+  },
+  async headers() {
+    return [
+      // {
+      //   source: '/:path((?!api/auth|_next/static).*)',
+      //   headers: [
+      //     { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' },
+      //   ],
+      // },
+      // {
+      //   // Agregar encabezados a los archivos estáticos
+      //   source: '/_next/static/:path*',
+      //   headers: [
+      //     { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
+      //     { key: 'Content-Encoding', value: 'gzip' },
+      //   ],
+      // },
+      {
+        // Cache para las páginas generadas
+        source: '/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=0, s-maxage=86400, must-revalidate' },
+        ],
+      },
+    ];
   },
 };
