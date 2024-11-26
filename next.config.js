4961771927b9d195063650a6ac43199ce692b40e
Estefania <estefania.osses.v@gmail.com>
2024-11-25 22:38:52 -0300

    Dont ignore next/static and set distDir

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   cacheHandler: require.resolve('./src/utils/cache-handler.js'),
   output: 'standalone',
-  // distDir: '.next', // Mantener el estándar.
+  distDir: '.next', // Mantener el estándar.
   compress: true, // Habilita compresión por defecto.
   images: {
     remotePatterns: [
