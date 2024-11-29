a8a0782b3ccf3a8e14e9a2aadff87e9553cd9319
Estefania <estefania.osses.v@gmail.com>
2024-11-28 22:10:52 -0300

    Add specialities to selects data

---
+++ src/utils/managedata.js
@@ -0,0 +1,45 @@
+const cleanRut = rut => {
+  return typeof rut === 'string'
+    ? rut.replace(/^0+|[^0-9kK]+/g, '').toUpperCase()
+    : ''
+}
+
+export const formatRut = (rut) => {
+  rut = cleanRut(rut)
+
+  let result;
+  result = rut.slice(-4, -1) + '-' + rut.substr(rut.length - 1)
+  for (let i = 4; i < rut.length; i += 3) {
+    result = rut.slice(-3 - i, -i) + '.' + result
+  }
+  return result
+}
+
+const validarRut = rut => {
+  rut = rut.replace(/\./g, '').replace(/-/g, '');
+
+  const cuerpo = rut.slice(0, -1);
+  const dv = rut.slice(-1).toUpperCase();
+
+  let suma = 0;
+  let multiplo = 2;
+
+  for (let i = cuerpo.length - 1; i >= 0; i--) {
+    suma += parseInt(cuerpo.charAt(i), 10) * multiplo;
+    multiplo = (multiplo < 7) ? multiplo + 1 : 2;
+  }
+
+  const resto = suma % 11;
+  let dvEsperado = 11 - resto;
+
+  dvEsperado = dvEsperado === 11 ? '0' : dvEsperado === 10 ? 'K' : dvEsperado.toString();
+
+  return dvEsperado === dv;
+}
+
+const rutEjemplo = '12.345.678-5';
+if (validarRut(rutEjemplo)) {
+  console.log('RUT válido');
+} else {
+  console.log('RUT inválido');
+}
