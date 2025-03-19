741b4200d71ffa9d5b5fcb281c7b4ec3fbc53cab
Estefania <estefania.osses.v@gmail.com>
2025-03-19 15:07:40 -0300

    Fix: add proxy to img and files

--- src/components/Parser.jsx
+++ src/components/Parser.jsx
@@ -4,22 +4,49 @@ import parse, { domToReact } from 'html-react-parser';
 const ParserImgToImage = ({ htmlContent, classType }) => {
   // Definir el transformador que convierte <img> en <Image>
 
+  const normalizarTexto = (texto) => {
+    // Expresiones regulares dinámicas para base y key
+    const baseRegex = new RegExp(`(${process.env.NEXT_PUBLIC_BASE_IMG})`, "i");
+    const keyRegex = new RegExp(`(/${process.env.NEXT_PUBLIC_KEY_IMG}/i)`, "i");
+
+    // Expresión regular para la URL (nombre de archivo de imagen con extensión)
+    const urlRegex = /(\b\w+\.(jpg|png|gif|jpeg|webp)\b)/i;
+
+    // Extraer las partes
+    const baseMatch = texto.match(baseRegex) || process.env.NEXT_PUBLIC_BASE_IMG;
+    const urlMatch = texto.match(urlRegex);
+    const keyMatch = texto.match(keyRegex) || process.env.NEXT_PUBLIC_KEY_IMG;
+
+    // Obtener los valores únicos (en caso de que haya duplicados)
+    const base = typeof baseMatch === 'string' ? baseMatch : baseMatch[1];
+    const url = urlMatch[1];
+    const key = typeof keyMatch === 'string' ? keyMatch : keyMatch[1];
+
+    // Reconstruir el texto en el orden correcto
+    return `${base}${url}${key}`;
+  }
+
   const prepareImg = (src) => {
-    console.log('SRC', src)
+    const match_base = src.match(new RegExp(process.env.NEXT_PUBLIC_BASE_IMG)) || [];
+    const match_key = src.match(new RegExp(/(process.env.NEXT_PUBLIC_KEY_IMG)/i)) || []
 
-    // const match_base = src.match(new RegExp(process.env.NEXT_PUBLIC_BASE_IMG, 'g'))
-    // const match_key = src.match(new RegExp(process.env.NEXT_PUBLIC_KEY_IMG, 'g'))
-
-    /* if(src.match(new Reg)){
-
-    } else */ if (src.includes(process.env.NEXT_PUBLIC_BASE_IMG) && src.includes(process.env.NEXT_PUBLIC_KEY_IMG)) {
-      return src
-    } else if (src.includes(process.env.NEXT_PUBLIC_BASE_IMG)) {
-      return `${src}${process.env.NEXT_PUBLIC_KEY_IMG}`
-    } else if (src.includes(process.env.NEXT_PUBLIC_KEY_IMG)) {
-      return `${process.env.NEXT_PUBLIC_BASE_IMG}${src}`
+    if (match_base.length > 1 || match_key.length > 1) {
+      normalizarTexto(src)
+    } else if (src.includes(process.env.NEXT_PUBLIC_BASE_IMG) && src.includes('https://reposaludmental.blob.core.windows.net/test/') && !src.includes(process.env.NEXT_PUBLIC_KEY_IMG)) {
+  
+      return `/api/file-proxy?filePath=${src}`
+    } else if (src.includes(process.env.NEXT_PUBLIC_BASE_IMG) && src.includes(process.env.NEXT_PUBLIC_KEY_IMG)) {
+      const removeKey = src.split('?')[0]
+      return `/api/file-proxy?filePath=${removeKey}`
+    } else if (src.includes(process.env.NEXT_PUBLIC_BASE_IMG) && !src.includes(process.env.NEXT_PUBLIC_KEY_IMG)) {
+  
+      return `/api/file-proxy?filePath=${src}`
+    } else if (src.includes(process.env.NEXT_PUBLIC_KEY_IMG) && !src.includes(process.env.NEXT_PUBLIC_BASE_IMG)) {
+  
+      return `/api/file-proxy?filePath=${process.env.NEXT_PUBLIC_BASE_IMG}${src}`
     } else if (!src.includes(process.env.NEXT_PUBLIC_BASE_IMG) && !src.includes(process.env.NEXT_PUBLIC_KEY_IMG)) {
-      return `${process.env.NEXT_PUBLIC_BASE_IMG}${src}${process.env.NEXT_PUBLIC_KEY_IMG}`
+  
+      return `/api/file-proxy?filePath=${process.env.NEXT_PUBLIC_BASE_IMG}${src}`
     }
   }
 
