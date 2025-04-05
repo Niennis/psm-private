786a87ef6c5cbe3b55c1e237b0a12f9a61ab70b9
Estefania <estefania.osses.v@gmail.com>
2025-04-05 17:04:10 -0300

    Fix tokenizacion

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,13 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google"
-import { fetchUser, fetchUsers, fetchUserByEmail } from "@/services/UsersServices";
-import { redirect } from "next/dist/server/api-utils";
-import bcrypt from "bcryptjs"
-import { fetchUserMailAndPass } from "@/services/UsersServices";
-import Credentials from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { fetchUser, fetchUsers, fetchUserByEmail, fetchUserMailAndPass } from "@/services/UsersServices";
 import { fetchProfessionals } from "@/services/DoctorsServices";
 import CacheHandler from "@/utils/cache-handler";
 
+const cacheHandler = new CacheHandler();
+
 const searchUser = async (email) => {
-  
   const cacheKey = `user-${email}`;
   const cachedUser = await cacheHandler.get(cacheKey);
   if (cachedUser) return cachedUser;
@@ -18,18 +16,18 @@ const searchUser = async (email) => {
     const [users, professionals, administrador] = await Promise.all([
       fetchUsers(),
       fetchProfessionals(),
-      fetchUserByEmail(email)
+      fetchUserByEmail(email),
     ]);
 
-    const user = users.users.find(user => user.email === email);
-    const professional = professionals.find(prof => prof.email === email);
-
+    const user = users.users.find((user) => user.email === email);
+    const professional = professionals.find((prof) => prof.email === email);
     const foundUser = user || professional || administrador;
+
     if (foundUser) {
       // await cacheHandler.set(cacheKey, foundUser, { tags: ['users'] });
       return foundUser;
     } else {
-      console.error("No se encontró el usuario:", error);
+      console.error("No se encontró el usuario:", email);
       throw new Error("No se encontró el usuario");
     }
   } catch (error) {
@@ -38,137 +36,103 @@ const searchUser = async (email) => {
   }
 };
 
-const cacheHandler = new CacheHandler();
-
 const authOptions = {
-  session: { 
+  debug: true,
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
     strategy: "jwt",
-    maxAge: 4 * 60 * 60,
+    maxAge: 4 * 60 * 60, // 4 horas
   },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       profile(profile) {
-        return ({
+        return {
           id: profile.sub,
           name: profile.name,
           apellido: profile.family_name,
           email: profile.email,
-          image: profile.picture
-        })
-      }
+          image: profile.picture,
+        };
+      },
     }),
     Credentials({
       credentials: {
-        email: { label: "email", type: "email", placeholder: 'example@example.com' },
-        password: { label: "password", type: "password" }
+        email: { label: "email", type: "email", placeholder: "example@example.com" },
+        password: { label: "password", type: "password" },
       },
-      /* AUTHORIZE */
       authorize: async (credentials) => {
-        // const cacheKey = `user-${credentials.email}`;
-        // const cachedUser = await cacheHandler.get(cacheKey);
-        // if (cachedUser) return cachedUser;
-
-        // let user = undefined;
-        const pwHash = credentials.password
-
-        let body = {
+        const body = {
           email: credentials.email,
-          contrasena: pwHash
-        }
+          contrasena: credentials.password,
+        };
 
         try {
-          const user = await fetchUserMailAndPass(body)
-          if (!user) {
-            throw new Error("usuario no encontrado.")
-          }
-
-          // if (user) {
-          //   await cacheHandler.set(cacheKey, user, { tags: ['user'] });
+          const user = await fetchUserMailAndPass(body);
+          if (!user) throw new Error("Usuario no encontrado.");
           return user;
-          // }
-          // throw new Error("Usuario no encontrado.");
-
-          // if (user.email === body.email && user.contrasena === body.contrasena) {
-          //   return user
-          // }
         } catch (error) {
-          // console.log('Ocurrió un problema: ', error)
-          throw new Error(`ocurrió un problema: ${error}`)
+          throw new Error(`Ocurrió un problema: ${error}`);
         }
       },
-      /* FIN AUTHORIZE */
     }),
   ],
   pages: {
-    signIn: '/',
-    error: '/error/page'
+    signIn: "/",
+    error: "/error/page",
   },
   callbacks: {
     async signIn({ account, profile, credentials }) {
       if (account.provider === "google") {
-        if (profile.email_verified && ( profile.email.endsWith("@mail.udp.cl") || profile.email.endsWith("@gmail.com"))) {
+        if (
+          profile.email_verified &&
+          (profile.email.endsWith("@mail.udp.cl") || profile.email.endsWith("@gmail.com"))
+        ) {
           const user = await searchUser(profile.email);
-          if (user) {
-            return true;
-          } else {
-            return false
-          }
+          return !!user;
         } else {
           throw new Error("Correo no autorizado o usuario no encontrado.");
-          return false
         }
       }
 
       if (account.provider === "credentials") {
         const body = { email: credentials.email, contrasena: credentials.password };
         const user = await fetchUserMailAndPass(body);
-        if (user) {
-          return true;
-        } else {
-          throw new Error("Credenciales incorrectas.");
-          return false
-        }
+        return !!user;
       }
 
       return false;
     },
+
     async jwt({ token, user }) {
-      const profile = await searchUser(token.email)
-      if (profile.validacion === false) {
-        throw new Error("Usuario no encontrado.");
-      } else {
-        if (token) {
-          token.id = profile.id
-          token.name = token.name || profile.nombre;
-          token.rol = profile.tipo_usuario;
-          return token;
+      if (user) {
+        const profile = await searchUser(user.email);
+        if (profile?.validacion === false) {
+          throw new Error("Usuario no encontrado.");
         }
+
+        token.id = profile.id;
+        token.name = profile.nombre || user.name;
+        token.rol = profile.tipo_usuario;
+        token.email = profile.email;
       }
+
+      return token;
     },
-    async session({ session, user, token }) {
-      // const cacheKey = `session-${session.user.email}`;
-      // const cachedSession = await cacheHandler.get(cacheKey);
-      // if (cachedSession) return cachedSession;
-      session.user = token;
-      session.user.id = token.id;
-      session.user.name = token.name
-      session.user.rol = token.rol;
-      // try {
-      //   // Guardar en caché
-      //   await cacheHandler.set(cacheKey, session, { tags: ['sessions'] });
-      //   return session;
-      // } catch (error) {
-      //   console.error("Error al generar sesión:", error);
-      // }
-      return session
+
+    async session({ session, token }) {
+      session.user = {
+        id: token.id,
+        name: token.name,
+        rol: token.rol,
+        email: token.email,
+      };
+      return session;
     },
   },
-  secret: process.env.NEXTAUTH_SECRET,
 };
 
-// export { handler as GET, handler as POST }
 const handler = NextAuth(authOptions);
 export { authOptions };
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
