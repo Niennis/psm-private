a21efd4baae05eed47813edc6a21030381ed1ed5
Estefania <estefania.osses.v@gmail.com>
2026-03-31 01:42:58 -0300

    enable MSW server in production

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -1,5 +1,5 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.NODE_ENV === 'development') {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { server } = await import('./mocks/server')
     server.listen({ onUnhandledRequest: 'bypass' })
     console.log('MSW Server started')
